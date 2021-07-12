770108fb0f305418a74026fb3c600b1cb498c850
İlker <36369727+ilkerkanber@users.noreply.github.com>
2021-07-12 14:46:49 +0300

    Initialize

---
+++ ZigZag/Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject spawnObject;
+    [SerializeField] Transform startLocation;
+    Vector3 currentLocation;
+    void Start()
+    {
+        
+    }
+    void Setup()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
